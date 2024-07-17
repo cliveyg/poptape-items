7e362ed01590b8396bc0a16e4afe55d262358acc
cliveyg <junkcat@gmail.com>
2024-07-17 11:10:42 -0300

    Add bulk fetch tests

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -197,15 +197,26 @@ class MyTest(FlaskTestCase):
         self.assertEqual(returned_item1.get('name'), data1.get('name'))
         self.assertEqual(returned_item1.get('category'), data1.get('category'))
 
-    def test_fail_bulk_fetch_bad_inputs(self):
+    def test_fail_bulk_fetch_bad_inputs_1(self):
         create_json = { 'item_ids': ["some-blah", str(uuid.uuid4())]}
         headers = {'Content-type': 'application/json'}
 
         response = self.client.post('/items/bulk/fetch', headers=headers, json=create_json)
         returned_message = response.json
-        self.app.logger.info("RETUNRNED MESSAGE IS %s", returned_message)
+        expected_message = {'error': "'some-blah' is too short", 'message': 'Check ya inputs mate.'}
+        self.assertDictEqual(returned_message, expected_message)
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(1, 0)
+
+    def test_fail_bulk_fetch_bad_inputs_1(self):
+        create_json = { 'item_ids': ["bca9ee07-e4c8-49ff-b7ee-c1d697d14c9x", str(uuid.uuid4())]}
+        headers = {'Content-type': 'application/json'}
+
+        response = self.client.post('/items/bulk/fetch', headers=headers, json=create_json)
+        returned_message = response.json
+        self.app.logger.info("RETURNED MESSAGE IS %s", returned_message)
+        expected_message = {'error': "'some-blah' is too short", 'message': 'Check ya inputs mate.'}
+        self.assertDictEqual(returned_message, expected_message)
+        self.assertEqual(response.status_code, 400)
 
     def test_create_item_fail_name_too_short(self):
         headers = {'Content-type': 'application/json', 'x-access-token': 'somefaketoken'}
