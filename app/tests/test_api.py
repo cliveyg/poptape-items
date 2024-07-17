086988b866c417c9d2f06f58971dc2478eb5b566
cliveyg <junkcat@gmail.com>
2024-07-17 12:22:19 -0300

    Add more bulk fetch tests

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -224,8 +224,7 @@ class MyTest(FlaskTestCase):
 
         response = self.client.post('/items/bulk/fetch', headers=headers, json=create_json)
         returned_message = response.json
-        self.app.logger.info("RET MESS: %s", returned_message)
-        expected_message = {'error': "'bca9ee07-e4c8-49ff-b7ee-c1d697d14c9x' does not match '[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}'",
+        expected_message = {'error': "Additional properties are not allowed ('something' was unexpected)",
                             'message': 'Check ya inputs mate.'}
         self.assertDictEqual(returned_message, expected_message)
         self.assertEqual(response.status_code, 400)
