c77ea3bfb01271381fa8cb0c19aa61be80cdbb6b
cliveyg <junkcat@gmail.com>
2024-07-17 12:58:02 -0300

    Add get items by user tests

--- app/tests/fixtures.py
+++ app/tests/fixtures.py
@@ -3,5 +3,8 @@ import uuid
 def getPublicID():
     return str(uuid.uuid4())
 
+def getSpecificPublicID():
+    return "e3cf14c3-df06-4360-af93-b445c3d78d9e"
+
 def exceptionFactory(exception, message):
     return exception(message)
