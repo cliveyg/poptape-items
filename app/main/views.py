d5b71ebd12b505311e4254165cce54c540eed666
cliveyg <junkcat@gmail.com>
2024-07-24 14:53:05 -0300

    Add more tests

--- app/main/views.py
+++ app/main/views.py
@@ -154,11 +154,11 @@ def get_items_by_user(public_id, request):
         app.logger.error("Error: [%s]", e)
         return jsonify({'message': 'There\'s a problem with your arguments, the db, both or something else ;)'}), 400
 
-    if results_count == 0:
-        return jsonify({'message': 'Nowt ere chap'}), 404
-
     if results_count <= offset:
         return jsonify({'message': 'offset is too big'}), 400
+
+    if results_count == 0:
+        return jsonify({'message': 'Nowt ere chap'}), 404
 
     last_id = starting_id[offset]['_id']
 
